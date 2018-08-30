009f4f3f186a676c6fd290a93095d928530c600a
coder-shankar <34474567+coder-shankar@users.noreply.github.com>
2018-08-30 07:00:16 +0545

    Update knexfile.js

--- src/knexfile.js
+++ src/knexfile.js
@@ -1,11 +1,12 @@
 require("babel-register");
 require("dotenv").config({ path: __dirname + "/../.env" });
 
+/**
+ * Database configuration.
+ */
 module.exports = {
   client: process.env.DB_CLIENT,
-
   connection: process.env.DATABASE_URL,
-
   migrations: {
     tableName: "migrations",
     directory: "./migrations",
